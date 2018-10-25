c3668c5ef2013828ec84791c2850581bbe0fd01d
Mrinal <waliamrinal@gmail.com>
2018-10-26 01:28:19 +0530

    to predict the output of a scattered function

---
+++ predic_sklearn.py
@@ -0,0 +1,11 @@
+from sklearn.naive_bayes import GaussianNB
+
+import numpy as np
+
+x = np.array([[11,141],[22,145],[293,2]])
+y = np.array([188,245,123])
+
+clf = GaussianNB()
+clf.fit(x,y)
+
+print(clf.score(x,y))
